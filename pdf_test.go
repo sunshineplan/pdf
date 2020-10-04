eeef988dc3c9ca2193059899982d129c834dcfe8
sunshineplan <sunshineplan@users.noreply.github.com>
2020-10-04 16:46:13 +0800

    Fix test

--- pdf_test.go
+++ pdf_test.go
@@ -1,20 +1,21 @@
 package pdf
 
 import (
-	"os"
+	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
 func TestDecode(t *testing.T) {
-	f, err := os.Open("video-001.pdf")
+	b, err := ioutil.ReadFile("video-001.pdf")
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if _, err := Decode(f); err != nil {
-		t.Error("Failed to decode pdf")
+	if _, err := Decode(bytes.NewBuffer(b)); err != nil {
+		t.Error("Failed to decode pdf", err)
 	}
-	if _, err := DecodeConfig(f); err != nil {
-		t.Error("Failed to decode pdf config")
+	if _, err := DecodeConfig(bytes.NewBuffer(b)); err != nil {
+		t.Error("Failed to decode pdf config", err)
 	}
 }
